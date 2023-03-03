125f01323563a4e9e374c9f852c38aebeafe19a3
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2023-03-03 14:13:23 -0300

    First implementation of basic static version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function ProductCategoryRow({ category }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>{category}</td>
+    </tr>
+  );
+}
+
+function ProductRow({ product }) {
+  return (
+    <tr>
+      <td> {product.name} </td>
+      <td> {product.price} </td>
+    </tr>
+  );
+}
+
+function ProductTable({ products }) {
+  const fruits = products
+    .filter((product) => (product.category = "Fruits"))
+    .map((product) => <ProductRow product={product} />);
+  const vegetables = products
+    .filter((product) => (product.category = "vegetables"))
+    .map((product) => <ProductRow product={product} />);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ProductCategoryRow category={"Fruits"} />
+        {fruits}
+        <ProductCategoryRow category={"Vegetables"} />
+        {vegetables}
+      </tbody>
+    </table>
+  );
+}
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="Search..." />
+      <label for="checkbox">Only show products in stock</label>
+      <input id="checkbox" type="checkbox"></input>
     </div>
   );
 }
 
-export default App;
+function FilterableProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+}
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
